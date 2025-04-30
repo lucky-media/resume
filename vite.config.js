74d89d2eede110933f8ab78aa8bad5350070df84
Afan417 <batman.two@hotmail.com>
2025-04-30 10:45:56 +0200

    Update starter-kit.yaml and vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    plugins: [laravel(["resources/css/tailwind.css", "resources/js/site.js"])],
+    plugins: [
+        tailwindcss(),
+        laravel(["resources/css/tailwind.css", "resources/js/site.js"]),
+    ],
 });
